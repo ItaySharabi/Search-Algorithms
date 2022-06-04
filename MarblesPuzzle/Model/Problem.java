2e0e0c48daa4f0d5b3a59f5374b9dd73a0f7e178
Itay Sharabi <itay.nextweb@gmail.com>
2022-06-04 16:22:07 +0300

    Documentation

--- MarblesPuzzle/Model/Problem.java
+++ MarblesPuzzle/Model/Problem.java
@@ -1,7 +1,11 @@
 package MarblesPuzzle.Model;
 
+import API.Algorithm;
+import API.HeuristicEval;
 import API.IProblem;
 import API.IState;
+import Algorithms.*;
+import MarblesPuzzle.Heuristics.ManhattanDistance;
 
 public class Problem implements IProblem {
     private final IState start, goal;
@@ -19,6 +23,37 @@ public class Problem implements IProblem {
     @Override
     public IState getGoalState() {
         return goal;
+    }
+
+    @Override
+    public String solve(String algoName, boolean verbose) {
+        Algorithm algo;
+        HeuristicEval heuristicEval = new ManhattanDistance(getGoalState());
+        if (algoName.equalsIgnoreCase("BFS")) {
+            algo = new BFS(this, verbose);
+        }
+        else if(algoName.equalsIgnoreCase("DFID")) {
+            algo = new DFID(this, verbose);
+
+        }
+        else if(algoName.equalsIgnoreCase("A*") || algoName.equalsIgnoreCase("AStar")) {
+            algo = new AStar(this, verbose, heuristicEval);
+
+        }
+        else if(algoName.equalsIgnoreCase("IDA*") || algoName.equalsIgnoreCase("IDAStar")) {
+            algo = new IDAStar(this, verbose, heuristicEval);
+
+        }
+        else if(algoName.equalsIgnoreCase("DFBnB")) {
+            algo = new DFBnB(this, verbose, heuristicEval);
+
+        }
+        else {
+            System.out.println("Invalid algorithm name!");
+            return "";
+        }
+
+        return algo.execute();
     }
 
     @Override
